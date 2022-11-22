77f0e8e692c1009c20312c5549cab765e19a02fd
unknown <cogjava2953@iiht.tech>
2022-11-22 21:33:58 +0530

    Code refactoring

--- gateway-service/src/main/java/com/cognizant/gateway/filter/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/cognizant/gateway/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import com.cognizant.gateway.util.JwtUtil;
@@ -27,20 +29,23 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             if (!request.getHeaders().containsKey("Authorization")) {
-                ServerHttpResponse response = exchange.getResponse();
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
 
-                return response.setComplete();
+//                ServerHttpResponse response = exchange.getResponse();
+//                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+//
+//                return response.setComplete();
+                throw new AuthenticationServiceException("Failed to authorize, no authorization header found");
             };
 
             String token = request.getHeaders().getOrEmpty("Authorization").get(0);
             try {
                 jwtUtil.validateToken(token);
             } catch (Exception ex) {
-                ServerHttpResponse response = exchange.getResponse();
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
-
-                return response.setComplete();
+//                ServerHttpResponse response = exchange.getResponse();
+//                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+//
+//                return response.setComplete();
+                throw new AuthenticationServiceException("Failed to authorize, invalid jwt token provided");
             }
 
             Claims claims = jwtUtil.getClaims(token);
