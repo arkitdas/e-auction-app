4587162931077b9b44866447eda1b88622b347e0
arkit <arkitdas1@gmail.com>
2022-11-23 04:20:56 +0530

    Refactoring

--- gateway-service/src/main/java/com/cognizant/gateway/filter/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/cognizant/gateway/filter/JwtAuthenticationFilter.java
@@ -79,7 +79,7 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             if (!(path.contains("/seller") && role.equals("ROLE_SELLER")) &&
                     !(path.contains("/buyer") && role.equals("ROLE_BUYER")) && 
                     !(path.contains("/product") && (role.equals("ROLE_BUYER") || role.equals("ROLE_SELLER")))) {
-            	ApiResponse response = ApiResponse.ofFailure(HttpStatus.UNAUTHORIZED.value(), "Unauthorized Access, Invalid token");
+            	ApiResponse response = ApiResponse.ofFailure(HttpStatus.FORBIDDEN.value(), "Unauthorized Access, Invalid token");
             	
                 ServerHttpResponse httpResponse = exchange.getResponse();
                 httpResponse.setStatusCode(HttpStatus.FORBIDDEN);
